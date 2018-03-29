dbc4cba8e9f9e656dc88a8458b85f25bf701fadf
Christoph Lambio <c.lambio@datalyze-solutions.com>
2018-03-29 16:56:46 +0200

    Database is accessed and the right tile is served.

--- lib/ios/AirMaps/AIRMapLocalTileOverlay.m
+++ lib/ios/AirMaps/AIRMapLocalTileOverlay.m
@@ -17,17 +17,26 @@
 
 -(void)loadTileAtPath:(MKTileOverlayPath)path result:(void (^)(NSData *, NSError *))result {
     NSMutableString *tileFilePath = [self.URLTemplate mutableCopy];
-    
-    NSString *pathDummyData = [[NSBundle mainBundle] pathForResource:@"trails" ofType:@"mbtiles"];
+    // https://gist.github.com/tmcw/4954720
+    // http://www.maptiler.org/google-maps-coordinates-tile-bounds-projection/
+    NSString *pathDummyData = [[NSBundle mainBundle] pathForResource:@"dhaka2" ofType:@"mbtiles"];
     FMDatabase *offlineDataDatabase = [FMDatabase databaseWithPath:pathDummyData];
     [offlineDataDatabase open];
     NSMutableString *query = [NSMutableString stringWithString: @"SELECT * FROM map INNER JOIN images ON map.tile_id = images.tile_id WHERE map.zoom_level = {z} AND map.tile_column = {x} AND map.tile_row = {y};"];
+    
     [query replaceCharactersInRange: [query rangeOfString: @"{z}"] withString:[NSString stringWithFormat:@"%li", path.z]];
     [query replaceCharactersInRange: [query rangeOfString: @"{x}"] withString:[NSString stringWithFormat:@"%li", path.x]];
-    [query replaceCharactersInRange: [query rangeOfString: @"{y}"] withString:[NSString stringWithFormat:@"%li", path.y]];
+    NSInteger y = (int)pow((double)2, (double)path.z) - path.y - 1;
+    [query replaceCharactersInRange: [query rangeOfString: @"{y}"] withString:[NSString stringWithFormat:@"%li", y]];
     // FMResultSet *databaseResult = [offlineDataDatabase executeQuery:@"SELECT * FROM map INNER JOIN images ON map.tile_id = images.tile_id WHERE map.zoom_level = 13 AND map.tile_column = 1506 AND map.tile_row = 5376"];
     FMResultSet *databaseResult = [offlineDataDatabase executeQuery:query];
     NSLog(@"%@", databaseResult);
+    NSLog(@"Query: %@", query);
+    NSLog(@"z: %li", path.z);
+    NSLog(@"x: %li", path.x);
+    NSLog(@"y: %li", path.y);
+    NSLog(@"yy: %li", y);
+    
     if ([databaseResult next]) {
         NSDictionary *dict = [databaseResult resultDictionary];
         NSLog(@"Dictionary: %@", dict);
